3d1554a301b6f33fdc22ed9cbc32fb2a119964ff
demola malomo <malomo.alaba@yahoo.com>
2021-05-23 13:42:52 +0100

    custom hooks and reuse

--- src/pages/Albums.tsx
+++ src/pages/Albums.tsx
@@ -1,49 +1,87 @@
 import Header from '../components/Header';
 import { Flex, Box } from '@chakra-ui/react';
 import AlbumIcon from '../svgs/AlbumIcon';
+import useFetch from '../hooks/useFetch.hook'; //add
+
+//add
+interface IAlbum {
+	userId: number;
+	id: number;
+	title: string;
+}
 
 const Albums = () => {
+	//add
+	const [responses, isError, isLoading] = useFetch<IAlbum>('albums');
+	if (isLoading) {
+		<Box
+			mt='1'
+			fontWeight='bold'
+			fontSize='xl'
+			as='h6'
+			isTruncated
+			color='teal.400'
+		>
+			Loading Albums......
+		</Box>;
+	}
 	return (
 		<div>
 			<Header />
 			<Box mt='20' mx='auto' width={{ sm: '100%', lg: '80%' }}>
-				<Box w='100%' borderWidth='1px' borderRadius='lg'>
-					<Flex direction={{ lg: 'row' }}>
-						<Flex
-							paddingLeft='3'
-							justifyContent='center'
-							alignItems='center'
-						>
-							<AlbumIcon />
-						</Flex>
-						<Box p='6' w={{ sm: '100%', lg: '80%' }}>
-							<Flex marginBottom='4'>
-								<Box mt='1' as='p' marginRight='2.5'>
-									UserId:
-								</Box>
-								<Box mt='1' as='p' fontWeight='bold'>
-									01
-								</Box>
-							</Flex>
-							<Flex marginBottom='4'>
-								<Box mt='1' as='p' marginRight='2.5'>
-									ID:
-								</Box>
-								<Box mt='1' as='p' fontWeight='bold'>
-									01
-								</Box>
-							</Flex>
-							<Flex marginBottom='4'>
-								<Box mt='1' as='p' marginRight='2.5'>
-									Title:
-								</Box>
-								<Box mt='1' as='p' fontWeight='bold'>
-									sunt qui excepturi placeat culpa
+				{isError ? (
+					<Box
+						mt='1'
+						fontWeight='bold'
+						fontSize='xl'
+						as='h6'
+						isTruncated
+						color='red'
+					>
+						Oop!!! Error getting albums
+					</Box>
+				) : (
+					//Modify this section
+					responses?.map((response) => (
+						<Box w='100%' borderWidth='1px' borderRadius='lg' key={response.id}>
+							<Flex direction={{ lg: 'row' }}>
+								<Flex
+									paddingLeft='3'
+									justifyContent='center'
+									alignItems='center'
+								>
+									<AlbumIcon />
+								</Flex>
+								<Box p='6' w={{ sm: '100%', lg: '80%' }}>
+									<Flex marginBottom='4'>
+										<Box mt='1' as='p' marginRight='2.5'>
+											UserId:
+										</Box>
+										<Box mt='1' as='p' fontWeight='bold'>
+											{response.userId}
+										</Box>
+									</Flex>
+									<Flex marginBottom='4'>
+										<Box mt='1' as='p' marginRight='2.5'>
+											ID:
+										</Box>
+										<Box mt='1' as='p' fontWeight='bold'>
+											{response.id}
+										</Box>
+									</Flex>
+									<Flex marginBottom='4'>
+										<Box mt='1' as='p' marginRight='2.5'>
+											Title:
+										</Box>
+										<Box mt='1' as='p' fontWeight='bold'>
+											{response.title}
+										</Box>
+									</Flex>
 								</Box>
 							</Flex>
 						</Box>
-					</Flex>
-				</Box>
+					))
+				)}
 			</Box>
 		</div>
 	);
--- src/pages/Photos.tsx
+++ src/pages/Photos.tsx
@@ -1,65 +1,105 @@
 import Header from '../components/Header';
 import { Flex, Box } from '@chakra-ui/react';
 import PhotoIcon from '../svgs/PhotoIcon';
+import useFetch from '../hooks/useFetch.hook'; //add
+
+//add
+interface IPhoto {
+	albumId: number;
+	id: number;
+	title: string;
+	url: string;
+	thumbnailUrl: string;
+}
 
 const Photos = () => {
+	//add
+	const [responses, isError, isLoading] = useFetch<IPhoto>('photos');
+	if (isLoading) {
+		<Box
+			mt='1'
+			fontWeight='bold'
+			fontSize='xl'
+			as='h6'
+			isTruncated
+			color='teal.400'
+		>
+			Loading Albums......
+		</Box>;
+	}
 	return (
 		<div>
 			<Header />
 			<Box mt='20' mx='auto' width={{ sm: '100%', lg: '80%' }}>
-				<Box w='100%' borderWidth='1px' borderRadius='lg'>
-					<Flex direction={{ lg: 'row' }}>
-						<Flex
-							paddingLeft='3'
-							justifyContent='center'
-							alignItems='center'
-						>
-							<PhotoIcon />
-						</Flex>
-						<Box p='6' w={{ sm: '100%', lg: '80%' }}>
-							<Flex marginBottom='4'>
-								<Box mt='1' as='p' marginRight='2.5'>
-									AlbumId:
-								</Box>
-								<Box mt='1' as='p' fontWeight='bold'>
-									01
-								</Box>
-							</Flex>
-							<Flex marginBottom='4'>
-								<Box mt='1' as='p' marginRight='2.5'>
-									ID:
-								</Box>
-								<Box mt='1' as='p' fontWeight='bold'>
-									01
-								</Box>
-							</Flex>
-							<Flex marginBottom='4'>
-								<Box mt='1' as='p' marginRight='2.5'>
-									Title:
-								</Box>
-								<Box mt='1' as='p' fontWeight='bold'>
-									sunt qui excepturi placeat culpa
-								</Box>
-							</Flex>
-							<Flex marginBottom='4'>
-								<Box mt='1' as='p' marginRight='2.5'>
-									url:
-								</Box>
-								<Box mt='1' as='p' fontWeight='bold'>
-									https://via.placeholder.com/600/92c952
-								</Box>
-							</Flex>
-							<Flex marginBottom='4'>
-								<Box mt='1' as='p' marginRight='2.5'>
-									ThumbnailUrl:
-								</Box>
-								<Box mt='1' as='p' fontWeight='bold'>
-									https://via.placeholder.com/150/92c952
+				{isError ? (
+					<Box
+						mt='1'
+						fontWeight='bold'
+						fontSize='xl'
+						as='h6'
+						isTruncated
+						color='red'
+					>
+						Oop!!! Error getting albums
+					</Box>
+				) : (
+					//Modify this section
+					responses?.map((response) => (
+						<Box w='100%' borderWidth='1px' borderRadius='lg' key={response.id}>
+							<Flex direction={{ lg: 'row' }}>
+								<Flex
+									paddingLeft='3'
+									justifyContent='center'
+									alignItems='center'
+								>
+									<PhotoIcon />
+								</Flex>
+								<Box p='6' w={{ sm: '100%', lg: '80%' }}>
+									<Flex marginBottom='4'>
+										<Box mt='1' as='p' marginRight='2.5'>
+											AlbumId:
+										</Box>
+										<Box mt='1' as='p' fontWeight='bold'>
+											{response.albumId}
+										</Box>
+									</Flex>
+									<Flex marginBottom='4'>
+										<Box mt='1' as='p' marginRight='2.5'>
+											ID:
+										</Box>
+										<Box mt='1' as='p' fontWeight='bold'>
+											{response.id}
+										</Box>
+									</Flex>
+									<Flex marginBottom='4'>
+										<Box mt='1' as='p' marginRight='2.5'>
+											Title:
+										</Box>
+										<Box mt='1' as='p' fontWeight='bold'>
+											{response.title}
+										</Box>
+									</Flex>
+									<Flex marginBottom='4'>
+										<Box mt='1' as='p' marginRight='2.5'>
+											url:
+										</Box>
+										<Box mt='1' as='p' fontWeight='bold'>
+											{response.url}
+										</Box>
+									</Flex>
+									<Flex marginBottom='4'>
+										<Box mt='1' as='p' marginRight='2.5'>
+											ThumbnailUrl:
+										</Box>
+										<Box mt='1' as='p' fontWeight='bold'>
+											{response.thumbnailUrl}
+										</Box>
+									</Flex>
 								</Box>
 							</Flex>
 						</Box>
-					</Flex>
-				</Box>
+					))
+				)}
 			</Box>
 		</div>
 	);
